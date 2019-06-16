816f340e36a66e1a950d501227fee9752f4ab30d
batistaTiago <ekyidag@gmail.com>
2019-06-16 18:09:23 -0300

    deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -32,28 +32,7 @@ class Api {
         this.express.use(BodyParser.urlencoded({ extended: true }))
         this.express.use(BodyParser.json())
         this.express.use(Api.errorHandler)
-        this.express.use(cors({origin: 'http://localhost:4200'}));
-        // // Add headers
-        // this.express.use(
-        //     (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-
-        //         // Website you wish to allow to connect
-        //         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-        //         // Request methods you wish to allow
-        //         res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-
-        //         // Request headers you wish to allow
-        //         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        //         // Set to true if you need the website to include cookies in the requests sent
-        //         // to the API (e.g. in case you use sessions)
-        //         // res.setHeader('Access-Control-Allow-Credentials', true);
-
-        //         // Pass to next layer of middleware
-        //         next();
-        //     }
-        // );
+        // this.express.use(cors({origin: 'http://localhost:4200'}));
 
         this.express.use(AuthConfig.config().initialize())
 
