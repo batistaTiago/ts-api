6c3a3e5c963497699d6cc89f72ebd3f9a517bb75
batistaTiago <ekyidag@gmail.com>
2019-06-16 13:42:24 -0300

    deploy

--- src/models/index.ts
+++ src/models/index.ts
@@ -8,7 +8,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const config = EnvironmentConfig.getSettings();
 const env = config.env || 'development';
-const extension = (env === 'development') ? '.ts' : '.js'
+const extension = (env === 'development') ? '.js' : '.js'
 const db: any = {};
 const modelRelations = require('./relations/relations')
 
