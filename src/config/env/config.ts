3b0d85412079c2d18ed5f7df82b298b936aa450a
batistaTiago <ekyidag@gmail.com>
2019-06-16 13:35:40 -0300

    deploy

--- src/config/env/config.ts
+++ src/config/env/config.ts
@@ -12,7 +12,17 @@ export class EnvironmentConfig {
         secret: '53cr3t'
     }
 
-    private static productionConfig = require('./production.env')
+    private static productionConfig = {
+        env: 'production',
+        serverPort: 3000,
+        db: 'db_portfolio',
+        username: 'epiz_24054376',
+        password: '6rVGM8lEPR68NQc',
+        dbURL: 'mysql://epiz_24054376@sql203.epizy.com:3306/epiz_24054376_db_portfolio',
+        host: 'sql203.epizy.com',
+        databasePort: 3306,
+        secret: 'ca6a6567c5d3eb55cff99b90fe868dc4'
+    }
     
     private static testConfig = {
         env: 'test',
